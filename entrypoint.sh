59b65dd6cd25a4a7f448dff7380f460b49b9f611
Sanjay Patel <sanjay10.patel@gmail.com>
2021-03-13 21:33:33 +0000

    Fix tee not using exit codes correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh -l
 
-set -o pipefail
-
 if [ ! -z $5 ]
 then
-  $GOPATH/bin/release $1 -username $2 -password $3 -repo $GITHUB_REPOSITORY -changelog $GITHUB_WORKSPACE/$4 -hash $GITHUB_SHA -provider github -host $5 | tee version.txt
+  version=$($GOPATH/bin/release $1 -username $2 -password $3 -repo $GITHUB_REPOSITORY -changelog $GITHUB_WORKSPACE/$4 -hash $GITHUB_SHA -provider github -host $5) || exit $?
 else
-  $GOPATH/bin/release $1 -username $2 -password $3 -repo $GITHUB_REPOSITORY -changelog $GITHUB_WORKSPACE/$4 -hash $GITHUB_SHA -provider github | tee version.txt
+  version=$($GOPATH/bin/release $1 -username $2 -password $3 -repo $GITHUB_REPOSITORY -changelog $GITHUB_WORKSPACE/$4 -hash $GITHUB_SHA -provider github) || exit $?
 fi
-echo "::set-output name=version::$(cat version.txt)"
+echo "::set-output name=version::$version"
